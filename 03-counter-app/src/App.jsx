31ae3d5434d6c2f90523937cd8fdcf2265a8df8f
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-05-23 13:20:05 -0500

    feat: props, proptypes and defaultprops

--- 03-counter-app/src/App.jsx
+++ 03-counter-app/src/App.jsx
@@ -1,9 +1,11 @@
+import { CounterApp } from "./CounterApp";
 import { FirstApp } from "./FirstApp";
 
 export const App = () => {
 	return (
 		<>
-			<FirstApp title={null} text={undefined} isLoading={false}>
+			<CounterApp value={8} />
+			<FirstApp title="FirstApp" text={undefined} isLoading={false}>
 				<p>Lorem ipsum dolor sit.</p>
 			</FirstApp>
 		</>
--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,14 +1,33 @@
-export const FirstApp = (props) => {
-	console.log(props);
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subtitle, children }) => {
 	const message = "puri";
-	const sayHello = () => "Hello world";
+	const sayHello = () => "Bienvenido!";
+
+	if (!title) {
+		throw {
+			message: "El title no existe",
+			code: 403,
+		};
+	}
 
 	return (
 		<>
-			<h1 className="title">FirstApp</h1>
+			<h1 className="title">{title}</h1>
 			<h2>{message.toUpperCase()}</h2>
-			<h3>{sayHello()}</h3>
-			{props.children}
+			<h3>{subtitle}</h3>
+			<h4>{sayHello()}</h4>
+			{children}
 		</>
 	);
+};
+
+FirstApp.propTypes = {
+	title: PropTypes.string.isRequired,
+	subtitle: PropTypes.string,
+	children: PropTypes.element.isRequired,
+};
+
+FirstApp.defaultProps = {
+	subtitle: "Hello",
 };
